289ce65aac3e6482a8e1639e41ba450cf3dc98eb
GauthamandMatt <GauthamandMatt@appacademy.io>
2016-02-16 11:17:03 -0500

    add pawn

--- test.rb
+++ test.rb
@@ -10,6 +10,10 @@ board.populate
 d = Display.new(board)
 
 board[[4, 4]] = Pawn.new([4,4], board, :white)
+board[[3, 3]] = Pawn.new([3,3], board, :white)
+board[[3, 5]] = Pawn.new([3,5], board, :black)
+board[[3, 4]] = Pawn.new([3,4], board, :white)
+board[[4, 4]].start = false
 # p board[[4, 4]].moves
 # p board[[4, 4]].moves.count
 p board[[4,4]].moves
